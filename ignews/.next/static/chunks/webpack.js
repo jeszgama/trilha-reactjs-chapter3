f3e5f6acd79ad2572eea49ff67651f33f249befa
“jessica” <jessicagama9827@gmail.com>
2021-10-29 13:35:54 -0400

    --configurando fonte externa

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d3f028d63e82fc7"; }
+/******/ 		__webpack_require__.h = function() { return "5d6d721dd768ce08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
