f2d5419114266ba38f0397914cb7258c35f3476e
“jessica” <jessicagama9827@gmail.com>
2021-10-29 13:59:37 -0400

    --title dinâmico por página

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d6d721dd768ce08"; }
+/******/ 		__webpack_require__.h = function() { return "002d63940c1670bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
