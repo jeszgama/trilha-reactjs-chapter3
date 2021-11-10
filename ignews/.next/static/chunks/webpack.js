1fb7a883ab2699aab3c5b4a114f7a173e2956f4b
“jessica” <jessicagama9827@gmail.com>
2021-11-10 09:48:30 -0400

    -- autenticação com next auth

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "252e6d23a12a080f"; }
+/******/ 		__webpack_require__.h = function() { return "b5d2c8ecf5d8daec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
