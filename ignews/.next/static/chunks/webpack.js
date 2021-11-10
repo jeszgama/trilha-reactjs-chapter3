52da8928a278d3f30b2e5b3d0b2d9ac3d9a0dd4e
“jessica” <jessicagama9827@gmail.com>
2021-11-10 10:46:16 -0400

    -- .

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "495dd2ee3e88cdc6"; }
+/******/ 		__webpack_require__.h = function() { return "6add8122e4ebba50"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
