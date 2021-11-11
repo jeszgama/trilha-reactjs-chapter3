0b806e8c53afdab816f577d104a4afcb20d02033
“jessica” <jessicagama9827@gmail.com>
2021-11-11 08:26:27 -0400

    -- usando faunaDB

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "985a0b4eaa03be4f"; }
+/******/ 		__webpack_require__.h = function() { return "560bc716d47f249a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- ignews/.next/static/webpack/webpack.06ebab53a1774538.hot-update.js
+++ ignews/.next/static/webpack/webpack.06ebab53a1774538.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "252e6d23a12a080f"; }
+/******/ 	__webpack_require__.h = function() { return "560bc716d47f249a"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
