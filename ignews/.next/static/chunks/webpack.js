d24338274f75002d558ba4484de41ac18aeb9bf2
“jessica” <jessicagama9827@gmail.com>
2021-11-17 09:40:12 -0400

    --Configurações no Github

--- ignews/.next/static/chunks/webpack.js
+++ ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "560bc716d47f249a"; }
+/******/ 		__webpack_require__.h = function() { return "5e6f3e24fa54b879"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- ignews/.next/static/webpack/webpack.099730b80520c49c.hot-update.js
+++ ignews/.next/static/webpack/webpack.099730b80520c49c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "560bc716d47f249a"; }
+/******/ 	__webpack_require__.h = function() { return "5e6f3e24fa54b879"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
